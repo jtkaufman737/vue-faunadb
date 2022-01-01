a8477b98ce502300cf8ecba379b2cc63d2f7f7e2
jtkaufman737 <jtkaufman737@gmail.com>
2022-01-01 15:44:43 -0500

    Final for finished version

--- src/main.js
+++ src/main.js
@@ -2,20 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import faunadb from 'faunadb'
 import './registerServiceWorker'
-import router from './router'
 
 const db = new faunadb.Client({
     secret: process.env.VUE_APP_FAUNADB_SECRET, 
     domain: 'db.us.fauna.com',
-    // port: 443,
-    // scheme: "https"
-});
+})
 
 Vue.config.productionTip = false
 Vue.prototype.$db = db
 Vue.prototype.$q = faunadb.query
 
 new Vue({
-  router,
   render: function (h) { return h(App) }
 }).$mount('#app')
